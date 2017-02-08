c0f7e5df5c98fa88eafc450867bf87fe1cb477f4
Randitya <rsmohamad@ust.hk>
2017-02-08 18:27:38 +0800

    initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+void init_all (void)
+{
+}
+
+int main (void)
+{
+    init_all ();
+
+    while (1);
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+//-- STDPERIPH INCLUDE --//
+#include <stm32f10x.h>
+
+//-- USER LIBRARY INCLUDES --//
+
+#endif //MAIN_H
